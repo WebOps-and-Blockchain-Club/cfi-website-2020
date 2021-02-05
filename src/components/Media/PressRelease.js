bc10e8a9d3967da1c708c8955b49404d20f7f73a
Devakrishna <devakrishna33@gmail.com>
2021-02-05 16:25:16 +0530

    Complted media page

--- src/components/Media/PressRelease.js
+++ src/components/Media/PressRelease.js
@@ -1,32 +1,48 @@
-import React from 'react'
-import data from './utils/data'
+import React, { Fragment } from "react";
+import { Row, Col } from "antd";
+import data from "./data";
 
 export default function PressRelease() {
-    return (
-        <>
-            {data.map(item => (
-                <div className="press-release-container">
-                    <div className="press-release-left">
-                        <h2>{item.pressCoverage}</h2>
-                    </div>
-                    <div className="press-release-right">
-                        {
-                            item.data.map(release => (
-                                <div className="press-release-right-items">
-                                    <a href={release.url} target="blank" className="press-release-headline">{release.headline}</a>
-                                    <p className="press-release-date">Posted on {release.date}</p>
-                                    <p className="press-release-publication"><b>Publication:</b> {release.publication}</p>
-                                    <p className="press-release-item"><b>Edition:</b> {release.edition}</p>
-                                    <p className="press-release-item"><b>Journalist:</b> {release.journalist}</p>
-                                    {release.student && <p className="press-release-item"><b>Students:</b> {release.student}</p>}
-                                    {release.professor && <p className="press-release-item"><b>Professor:</b> {release.professor}</p>}
-                                </div>
-                            
-                            ))
-                        }
-                    </div>
-                </div>
+  return (
+    <Fragment>
+      {data.map((item) => (
+        <Row className="press-release-container">
+          <Col width={24} className="press-release-right">
+            <h2 className="title">{item.pressCoverage}</h2>
+            {item.data.map((release) => (
+              <div className="press-release-right-items">
+                <a
+                  href={release.url}
+                  target="blank"
+                  className="press-release-headline"
+                >
+                  {release.headline}
+                </a>
+                <p className="press-release-date">Posted on {release.date}</p>
+                <p className="press-release-publication">
+                  <b>Publication:</b> {release.publication}
+                </p>
+                <p className="press-release-item">
+                  <b>Edition:</b> {release.edition}
+                </p>
+                <p className="press-release-item">
+                  <b>Journalist:</b> {release.journalist}
+                </p>
+                {release.student && (
+                  <p className="press-release-item">
+                    <b>Students:</b> {release.student}
+                  </p>
+                )}
+                {release.professor && (
+                  <p className="press-release-item">
+                    <b>Professor:</b> {release.professor}
+                  </p>
+                )}
+              </div>
             ))}
-            </>
-    )
+          </Col>
+        </Row>
+      ))}
+    </Fragment>
+  );
 }
